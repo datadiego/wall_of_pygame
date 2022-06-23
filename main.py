5acc491e4d44f83708692316818e091734cefe6a
Juan Diego <juandiegomariscal@gmail.com>
2022-06-23 12:48:56 +0200

    feat: añadida clase Emoji

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame as pg
-from resources import emoji
-
-WIDTH = 1080
-HEIGHT = 720
+from resources import WIDTH, HEIGHT
+from resources.emoji import Emoji
 
 class Animacion:
     def __init__(self):
         pg.init()
         self.pantalla = pg.display.set_mode((WIDTH, HEIGHT))
         #Podemos empezar a crear nuestros objetos aqui:
-
+        self.emoji = Emoji(self.pantalla)
+        self.clock = pg.time.Clock()
+        
     def bucle(self):
         loop = True
         while loop:
@@ -20,11 +20,16 @@ class Animacion:
                 if evento.type == pg.QUIT:
                     loop = False
             #Aqui situaremos nuestros objetos y sus metodos
-            self.ani()
+            self.emoji_ani()
+
+            #Termina nuestro loop
             pg.display.flip()
+            self.clock.tick(60)
     
-    def ani(self):
+    def emoji_ani(self):
         self.pantalla.fill((220,220,220))
+        self.emoji.mover()
+        self.emoji.muestra()
         pass
 
 wall_of_pygame = Animacion()
---
+++ resources/emoji.py
@@ -0,0 +1,50 @@
+import pygame as pg
+import random
+randint = random.randint
+import os
+
+from . import WIDTH, HEIGHT
+
+class Emoji:
+    def __init__(self, pantalla):
+        self.vel_x = randint(-3,3)
+        self.vel_y = randint(3,3)
+        self.pantalla = pantalla
+        self.img = pg.image.load(os.path.join("resources", "img", "emoji1.png"))
+        self.img = pg.transform.scale(self.img, (40, 40))
+        self.img2 = pg.image.load(os.path.join("resources", "img", "emoji2.png"))
+        self.img2 = pg.transform.scale(self.img2, (40, 40))
+        self.imgs = [self.img, self.img2]
+        self.current_img = self.imgs[0]
+        self.x = (WIDTH - self.img.get_width())/2
+        self.y = 200
+    def reset(self):
+        self.x = (WIDTH - self.img.get_width())/2
+        self.y = 200 
+    def check_border(self):
+        if self.x < 0:
+            self.next_emoji()
+            self.vel_x = -self.vel_x
+        if self.x > WIDTH-self.img.get_width():
+            self.next_emoji()
+            self.vel_x = -self.vel_x
+        if self.y < 0:
+            self.next_emoji()
+            self.vel_y = -self.vel_y
+        if self.y > HEIGHT -self.img.get_height():
+            self.next_emoji()
+            self.vel_y = -self.vel_y
+    def next_emoji(self):
+        index = self.imgs.index(self.current_img)
+        if index >= len(self.imgs)-1:
+            self.current_img = self.imgs[0]
+        else:
+            self.current_img = self.imgs[index+1]
+
+    def mover(self):
+        self.x += self.vel_x
+        self.y += self.vel_y
+        self.check_border()
+
+    def muestra(self):
+        self.pantalla.blit(self.current_img,(self.x, self.y))
