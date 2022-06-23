04b985c24ec77821b9897a7c82b4ffea573b2c61
Juan Diego <juandiegomariscal@gmail.com>
2022-06-23 16:27:38 +0200

    feat: Cambiadas varias lineas para cargar mas de una imagen por un for

--- resources/emoji.py
+++ resources/emoji.py
@@ -1,36 +1,35 @@
 from . import *
 randint = random.randint
 
-
-
-
 class Emoji:
     def __init__(self, pantalla):
+        archivos = ["emoji1.png", "emoji2.png"]
         self.vel_x = randint(-3,3)
         self.vel_y = randint(3,3)
         self.pantalla = pantalla
-        self.img = pg.image.load(os.path.join("resources", "img", "emoji1.png"))
-        self.img = pg.transform.smoothscale(self.img, (40, 40))
-        self.img2 = pg.image.load(os.path.join("resources", "img", "emoji2.png"))
-        self.img2 = pg.transform.smoothscale(self.img2, (40, 40))
-        self.imgs = [self.img, self.img2]
+        self.imgs = []
+        for imagen in archivos:
+            img = pg.image.load(os.path.join("resources", "img", imagen))
+            img = pg.transform.smoothscale(img, (40, 40))
+            self.imgs.append(img)
+
         self.current_img = self.imgs[0]
-        self.x = (WIDTH - self.img.get_width())/2
+        self.x = (WIDTH - self.current_img.get_width())/2
         self.y = 200
     def reset(self):
-        self.x = (WIDTH - self.img.get_width())/2
+        self.x = (WIDTH - self.current_img.get_width())/2
         self.y = 200 
     def check_border(self):
         if self.x < 0:
             self.next_emoji()
             self.vel_x = -self.vel_x
-        if self.x > WIDTH-self.img.get_width():
+        if self.x > WIDTH-self.current_img.get_width():
             self.next_emoji()
             self.vel_x = -self.vel_x
         if self.y < 0:
             self.next_emoji()
             self.vel_y = -self.vel_y
-        if self.y > HEIGHT -self.img.get_height():
+        if self.y > HEIGHT -self.current_img.get_height():
             self.next_emoji()
             self.vel_y = -self.vel_y
     def next_emoji(self):
